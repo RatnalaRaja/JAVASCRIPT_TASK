0cc286204a27c447b701468e6151ebc54387c178
Ratnala Raja <21mh1a4949@acoe.edu.in>
2024-05-28 20:48:03 +0530

    Using For Loop

--- Question 3/script.js
+++ Question 3/script.js
@@ -1,17 +1,36 @@
 var form =document.querySelector("form");
 
-var inp1=document.querySelector("#inp1");
-var inp2=document.querySelector("#inp2");
+// var inp1=document.querySelector("#inp1");
+// var inp2=document.querySelector("#inp2");
+
+var inps = document.querySelectorAll('input[type="text"]');
 var h4 = document.querySelector("h4");
 
-form.addEventListener("submit",function(e){
-    e.preventDefault();
+// form.addEventListener("submit",function(e){
+//     e.preventDefault();
 
-    if(inp1.value==="" || inp2.value===""){
-        h4.textContent="Error,Some Fields are Empty";
-        h4.style.color="red";
+//     if(inp1.value==="" || inp2.value===""){
+//         h4.textContent="Error,Some Fields are Empty";
+//         h4.style.color="red";
+//     }
+//     else{
+//         h4.textContent="";
+//     }
+// })
+
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    for(var i=0;i<inps.length;i++){
+        if(inps[i].value.trim()===''){
+
+            h4.textContent="Error,Some Fields Are Empty";
+            h4.style.color="red";
+            break;
+        }
+        else{
+            h4.textContent="";
+        }
     }
-    else{
-        h4.textContent="";
-    }
+
 })
